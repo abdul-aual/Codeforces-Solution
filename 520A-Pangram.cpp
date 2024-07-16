e235dfa2f0661fe79d81f8e0dece64ecbd0da8ae
abdul-aual <mdabdulaual38@gmail.com>
2024-07-16 10:56:25 +0600

    520A-Pangram codeforces solution

---
+++ 520A-Pangram.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string s;int n;
+    cin>>n;
+    cin >> s;
+
+    // Convert all characters to lowercase
+    for (int i = 0; i < n; i++) {
+        if (isupper(s[i])) {
+            s[i] = tolower(s[i]);
+        }
+    }
+
+    int m = 97;
+    bool found = true;
+
+    while (m <= 122) {
+        bool found_current_char = false;
+        for (int i = 0; i < n; i++) {
+            if ((int)s[i] == m) {
+                found_current_char = true;
+                break;
+            }
+        }
+        if (!found_current_char) {
+            found = false;
+            break;
+        }
+        ++m;
+    }
+
+    found? cout<<"YES"<<endl : cout<<"NO"<<endl;
+
+    return 0;
+}
